f4391c2b404ff2c0aab1c11b47f88e389ea1ab34
Mani2401e <enugula.000@gmail.com>
2024-10-06 18:46:53 +0530

    new2 commit

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,21 @@ app.use((req, res, next) => {
 // Routes
 app.use('/chatluu/users', require('./routes/userRoutes'));
 
+// ------deployment----------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production"){
+
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+  });
+} else {
+  app.get ("/", (req,res) => {
+    res.send("API is Running Successfully");
+  });
+}
+
 // Error handler middleware
 app.use(errorHandler);
 
